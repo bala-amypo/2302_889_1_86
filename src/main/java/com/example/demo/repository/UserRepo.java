89dda1b216a689a1028663827035141aafc338d6
bala-amypo <bala@amypo.in>
2025-12-18 07:48:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepo.java
+++ src/main/java/com/example/demo/repository/UserRepo.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
  import org.springframework.data.jpa.repository.JpaRepository;
  import org.springframework.stereotype.Repository;
  @Repository
- public interface UserRepository extends JpaRepository<UserEntity,Long>{
-    
+ public interface UserRepo extends JpaRepository<UserEntity,Long>{
+
  }
--- src/main/java/com/example/demo/service/impl/UserServiceImple.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImple.java
@@ -1,3 +1,13 @@
 package com.example.demo.service.impl;
 import com.example.demo.entity.UserEntity;
-import com.example.demo.repository.
+import com.example.demo.repository.UserRepo;
+import com.example.demo.service.UserService;
+import org.framework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImple implements UserService{
+    private final UserRepo userRepository;
+    @AutoWired
+}
