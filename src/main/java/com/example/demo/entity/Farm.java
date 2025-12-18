481b23cea87f014b96b3fc236a0e55576135bcb8
bala-amypo <bala@amypo.in>
2025-12-18 09:38:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/Farm.java
+++ src/main/java/com/example/demo/entity/Farm.java
@@ -1,1 +1,54 @@
 package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Farm {
+    @Id
+    private Long id;
+    private String name;
+    private Double soilPH;
+    private Double waterLevel ;
+    private String season;
+    
+    
+    public Farm() {
+    }
+    public Farm(Long id, String name, Double soilPH, Double waterLevel, String season) {
+        this.id = id;
+        this.name = name;
+        this.soilPH = soilPH;
+        this.waterLevel = waterLevel;
+        this.season = season;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getSoilPH() {
+        return soilPH;
+    }
+    public void setSoilPH(Double soilPH) {
+        this.soilPH = soilPH;
+    }
+    public Double getWaterLevel() {
+        return waterLevel;
+    }
+    public void setWaterLevel(Double waterLevel) {
+        this.waterLevel = waterLevel;
+    }
+    public String getSeason() {
+        return season;
+    }
+    public void setSeason(String season) {
+        this.season = season;
+    }
+}
