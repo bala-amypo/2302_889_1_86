6c6dbaba618f9c59d1ab9dd07f798706115ab7cb
bala-amypo <bala@amypo.in>
2025-12-18 08:13:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,7 @@
+package co.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncprder;
+import org.springframework.web.bind.annotataion.*;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserSer
