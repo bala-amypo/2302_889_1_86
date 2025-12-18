d9d876b0cac1c76e99725bd6063aaf45a8df109f
bala-amypo <bala@amypo.in>
2025-12-18 16:08:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,76 +1,47 @@
 package com.example.demo.entity;
+
 import jakarta.persistence.*;
-import lombok.*;
+import jakarta.validation.constraints.*;
 
 @Entity
-@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
- public class User{
+@Table(name = "users")
+public class User {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    @Column(name=unique)
+
+    @Email
+    @NotBlank
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @NotBlank
     private String password;
-    private String ADMIN;
-    private String USER;
-    
-    public User(Long id,String name,String email,String password,String aDMIN,String uSER){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
-        ADMIN=aDMIN;
-        USER=uSER;
 
-    }
-    public User(){
+    public User() {}
 
-    }
-    public Long getId(){
+    public Long getId() {
         return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
     }
-    public void setId(Long id){
-        this.id=id;
-    
-    }
-    public Long getName(){
-        return name;
 
-    }
-    public void setName(String name){
-        this.name=name;
-    
-    }
-    public Long getEmail(){
+    public String getEmail() {
         return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
     }
-    public void setEmail(String email){
-        this.email=email;
-    
-    }
-    public Long getPassword(){
+
+    public String getPassword() {
         return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
     }
-    public void setPassword(String Password){
-        this.password=password;
-    
-    }
-    public Long getADMIN(){
-        return ADMIN;
-
-    }
-    public void setADMIN(String aDMIN){
-            ADMIN=aDMIN;
-    }
-     public Long getUSER(){
-        return USER;
-
-    }
-    public void setUSER(String uSER){
-           USER=uSER;
-    }
- }
+}
