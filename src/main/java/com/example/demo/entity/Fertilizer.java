c31949e8cb9b4fd810f7e6ca6d29c50b55e0c7ce
bala-amypo <bala@amypo.in>
2025-12-18 09:53:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/Fertilizer.java
+++ src/main/java/com/example/demo/entity/Fertilizer.java
@@ -1,1 +1,57 @@
 package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+public class Fertilizer {
+    @Id
+    private Long id;
+    private String name;
+    private String npkRatio;
+    private String recommendedForCrops;
+    
+    public Fertilizer() {
+    }
+
+    public Fertilizer(Long id, String name, String npkRatio, String recommendedForCrops) {
+        this.id = id;
+        this.name = name;
+        this.npkRatio = npkRatio;
+        this.recommendedForCrops = recommendedForCrops;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNpkRatio() {
+        return npkRatio;
+    }
+
+    public void setNpkRatio(String npkRatio) {
+        this.npkRatio = npkRatio;
+    }
+
+    public String getRecommendedForCrops() {
+        return recommendedForCrops;
+    }
+
+    public void setRecommendedForCrops(String recommendedForCrops) {
+        this.recommendedForCrops = recommendedForCrops;
+    }
+
+    
+    
+}
--- src/main/java/com/example/demo/entity/Suggestion.java
+++ src/main/java/com/example/demo/entity/Suggestion.java
@@ -1,1 +1,59 @@
 package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*; 
+
+@Entity
+public class Suggestion {
+    @Id
+    private Long id;
+    private String suggestedCrops;
+    private String suggestedFertilizers;
+    private LocalDateTime createdAt;
+
+    public Suggestion() {
+    }
+
+    public Suggestion(Long id, String suggestedCrops, String suggestedFertilizers, LocalDateTime createdAt) {
+        this.id = id;
+        this.suggestedCrops = suggestedCrops;
+        this.suggestedFertilizers = suggestedFertilizers;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSuggestedCrops() {
+        return suggestedCrops;
+    }
+
+    public void setSuggestedCrops(String suggestedCrops) {
+        this.suggestedCrops = suggestedCrops;
+    }
+
+    public String getSuggestedFertilizers() {
+        return suggestedFertilizers;
+    }
+
+    public void setSuggestedFertilizers(String suggestedFertilizers) {
+        this.suggestedFertilizers = suggestedFertilizers;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+     
+
+}
