78e6be02cc0f620bd530719ff835a14358d20644
bala-amypo <bala@amypo.in>
2025-12-18 16:02:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/CropRepository.java
+++ src/main/java/com/example/demo/repository/CropRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repository;
- import com.example.demo.entity.User;
- import org.springframework.data.jpa.repository.JpaRepository;
- import org.springframework.stereotype.Repository;
- @Repository
- public interface UserRepository extends JpaRepository<User,Long>{
- }
+
+import com.example.demo.entity.Crop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CropRepository extends JpaRepository<Crop, Long> {
+
+    List<Crop> findSuitableCrops(Double ph, Double water, String season);
+}
--- src/main/java/com/example/demo/repository/FarmRepository.java
+++ src/main/java/com/example/demo/repository/FarmRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repository;
- import com.example.demo.entity.Farm;
- import org.springframework.data.jpa.repository.JpaRepository;
- import org.springframework.stereotype.Repository;
- @Repository
- public interface UserRepository extends JpaRepository<User,Long>{
- }
+
+import com.example.demo.entity.Farm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FarmRepository extends JpaRepository<Farm, Long> {
+
+    List<Farm> findByOwnerId(Long ownerId);
+}
