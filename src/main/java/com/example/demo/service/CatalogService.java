c9465ae43aa732dae40224ba4fd6869065e27bed
bala-amypo <bala@amypo.in>
2025-12-18 14:32:04 +0000

    User commit

--- src/main/java/com/example/demo/service/CatalogService.java
+++ src/main/java/com/example/demo/service/CatalogService.java
@@ -1,2 +1,9 @@
 package com.example.demo.service;
  
+import com.example.demo.entity.Crop;
+import com.example.demo.entity.Fertilizer;
+
+import java.util.List;
+public interface CatalogService{
+    Crop 
+}
