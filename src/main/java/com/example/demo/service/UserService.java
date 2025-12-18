6050c992b6b218554d633305f3eba61edc26330e
bala-amypo <bala@amypo.in>
2025-12-18 07:38:54 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,4 +3,6 @@ package com.example..demo.service;
 import com.example.demo.entity.UserEntity;
 import java.util.List;
 import java.util.Optional;
-public interface UserSer
+public interface UserService{
+    UserEntity saveUser
+}
