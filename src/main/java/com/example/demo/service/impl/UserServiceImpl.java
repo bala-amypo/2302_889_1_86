02b0d8a6e88da4fced41f302ce3dae6a9ee5fea5
bala-amypo <bala@amypo.in>
2025-12-19 03:59:50 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,39 +1,39 @@
 
-package com.example.demo.service.impl;
+// package com.example.demo.service.impl;
 
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+// import com.example.demo.entity.User;
+// import com.example.demo.repository.UserRepository;
+// import com.example.demo.service.UserService;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.server.ResponseStatusException;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+// import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-@Service
-@RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+// @Service
+// @RequiredArgsConstructor
+// public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+//     private final UserRepository userRepository;
 
-    @Override
-    public User register(User user) {
-        if (userRepository.existsByEmail(user.getEmail())) {
-            throw new ResponseStatusException(BAD_REQUEST, "Email");
-        }
-        user.setPassword(encoder.encode(user.getPassword()));
-        return userRepository.save(user);
-    }
+//     @Override
+//     public User register(User user) {
+//         if (userRepository.existsByEmail(user.getEmail())) {
+//             throw new ResponseStatusException(BAD_REQUEST, "Email");
+//         }
+//         user.setPassword(encoder.encode(user.getPassword()));
+//         return userRepository.save(user);
+//     }
 
-    @Override
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST, "User not found"));
-    }
+//     @Override
+//     public User findByEmail(String email) {
+//         return userRepository.findByEmail(email)
+//                 .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST, "User not found"));
+//     }
 
-    @Override
-    public User findById(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST));
-    }
-}
+//     @Override
+//     public User findById(Long id) {
+//         return userRepository.findById(id)
+//                 .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST));
+//     }
+// }
