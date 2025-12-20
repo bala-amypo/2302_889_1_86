b4d03f81c03955134e1859676478d026dbc76bc4
bala-amypo <bala@amypo.in>
2025-12-20 14:32:13 +0000

    User commit

--- src/main/java/com/example/demo/service/CatalogService.java
+++ src/main/java/com/example/demo/service/CatalogService.java
@@ -1,13 +1,13 @@
 // CatalogService.java - com.example.demo.service
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import com.example.demo.entity.Crop;
-import com.example.demo.entity.Fertilizer;
-import java.util.List;
+// import com.example.demo.entity.Crop;
+// import com.example.demo.entity.Fertilizer;
+// import java.util.List;
 
-public interface CatalogService {
-    Crop addCrop(Crop crop);
-    Fertilizer addFertilizer(Fertilizer fertilizer);
-    List<Crop> findSuitableCrops(Double ph, Double waterLevel, String season);
-    List<Fertilizer> findFertilizersForCrops(List<String> cropNames);
-}
+// public interface CatalogService {
+//     Crop addCrop(Crop crop);
+//     Fertilizer addFertilizer(Fertilizer fertilizer);
+//     List<Crop> findSuitableCrops(Double ph, Double waterLevel, String season);
+//     List<Fertilizer> findFertilizersForCrops(List<String> cropNames);
+// }
