e5f9423086c22a2a8fae9c36438d0421e8581533
bala-amypo <bala@amypo.in>
2025-12-26 13:49:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/FertilizerRepository.java
+++ src/main/java/com/example/demo/repository/FertilizerRepository.java
@@ -1,16 +1,13 @@
-// FertilizerRepository.java
 package com.example.demo.repository;
-
 import com.example.demo.entity.Fertilizer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface FertilizerRepository extends JpaRepository<Fertilizer, Long> {
-
-    @Query("select f from Fertilizer f where lower(f.recommendedForCrops) like %?1%")
-    List<Fertilizer> findByCropName(String cropName);
+    @Query("SELECT f FROM Fertilizer f WHERE LOWER(f.recommendedForCrops) LIKE LOWER(CONCAT('%', :crop, '%'))")
+    List<Fertilizer> findByCropName(@Param("crop") String crop);
 }
